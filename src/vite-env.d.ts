8ceaf5123c923318190dc474cb9ba0f79f618b6e
songjj <songjj@zetyun.com>
2023-03-17 16:19:31 +0800

    add:

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="vite/client" />
-<<<<<<< HEAD
-=======
 declare module 'qs'
->>>>>>> 24718178adee77e1802e3a5a6d044347a2637a0d
 
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
